05182055c7d13aef1a93cb41b0554eff477a23d0
Arkadiusz Koscik <lazior17@o2.pl>
2018-06-28 22:47:23 +0200

    start_activity + tło

---
+++ app/src/main/java/com/example/snowa/filmapps/StartActivity.java
@@ -0,0 +1,24 @@
+package com.example.snowa.filmapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button bstart=findViewById(R.id.bstart);
+        bstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(StartActivity.this, MenuActivity.class);
+                startActivity(intent);}
+        });
+    }
+}
